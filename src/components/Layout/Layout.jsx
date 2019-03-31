59281468a1f06530c219a64d6aa0211cab753a93
eMpoWer <jorgen.graff-n@outlook.com>
2019-03-31 23:01:16 +0200

    added the link to the about page in the layout.jsx

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -23,7 +23,7 @@ export default class Layout extends React.Component {
                         <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
                             <ul className="nav navbar-nav">
                                 <li className="active"><a href="index.html">Home</a></li>
-                                <li><a href="about.html">About</a></li>
+                                <li><a href="../pages/aboutpageabout.jsx/">About</a></li>
                                 <li><a href="../pages/contact-form-component/form-component.jsx">Contact</a></li>
                             </ul>
                         </div>
